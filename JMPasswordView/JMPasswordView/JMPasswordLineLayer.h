beb36cfe9f19ff39bebd840da8de25120f7e9022
Juuman <1149368562@qq.com>
2014-09-24 14:00:19 +0800

    JMPasswordView

---
+++ JMPasswordView/JMPasswordView/JMPasswordLineLayer.h
@@ -0,0 +1,15 @@
+//
+//  JMPasswordLineLayer.h
+//  JuumanUIKITDemo
+//
+//  Created by juuman on 14-9-22.
+//  Copyright (c) 2014年 juuman. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface JMPasswordLineLayer : CALayer
+@property (nonatomic,strong)NSArray *pointIds;
+@property (nonatomic)CGPoint nowPoint;
+
+@end
--- README222.md
+++
@@ -1,9 +0,0 @@
-JMPasswordView
-==============
-
-JMPasswordView is a GesturePassword view
-How To Use It
-==============
--(void)JMPasswordView:(JMPasswordView *)passwordView withPassword:(NSString *)password{
-    textLeb.text=password;
-}
